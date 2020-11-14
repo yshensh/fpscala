e3f623a2609e0bb654bfffed06b79f0aa6abcd8f
Yi Shen <yshensh@gmail.com>
2020-11-14 13:58:02 -0500

    add exercise 3.1 to 3.3

--- src/main/scala/fpscala/DataStructures.scala
+++ src/main/scala/fpscala/DataStructures.scala
@@ -1,5 +1,27 @@
 package fpscala
 
-object DataStructures {
+import fpscala.List._
 
+object DataStructures {
+  /***
+   * exercise 3.2
+   * Implement the function tail for removing the first element of a List
+   */
+  def tail[A](l: List[A]): List[A] =
+    l match {
+      case Nil => sys.error("tail of empty list")
+      case Cons(_, tail) => tail
+    }
+
+
+  /***
+   * exercise 3.3
+   * Implement the function setHead for replacing the first element of a List with a different value
+   */
+  def setHead[A](l: List[A], h: A): List[A] =
+    l match {
+      case Nil => sys.error("setHead on empty list")
+      case Cons(_, tail) => Cons(h, tail)
+    }
 }
+
--- src/test/scala/fpscala/DataStructuresTest.scala
+++ src/test/scala/fpscala/DataStructuresTest.scala
@@ -2,6 +2,7 @@ package fpscala
 
 import org.scalatest.{FunSuite, Matchers}
 import fpscala.List._
+import DataStructures._
 
 
 class DataStructuresTest extends FunSuite with Matchers {
@@ -13,6 +14,24 @@ class DataStructuresTest extends FunSuite with Matchers {
       case _ => 101 // otherwise x = 101
     }
     x should be (3)
+  }
+
+  test("exercise 3.2") {
+    tail(List(1, 2, 3)) shouldBe List(2, 3)
+
+    val caught = intercept[RuntimeException] {
+      tail(Nil)
+    }
+    assert(caught.getMessage.equals("tail of empty list"))
+  }
+
+  test("exercise 3.3") {
+    setHead(List(1, 2, 3), 0) shouldBe List(0, 2, 3)
+
+    val caught = intercept[RuntimeException] {
+      setHead(Nil, 0)
+    }
+    assert(caught.getMessage.equals("setHead on empty list"))
   }
 
 }
