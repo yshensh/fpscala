90650689f5c582e15aa0f74678050dd50d5f3553
Yi Shen <yshensh@gmail.com>
2020-11-14 11:57:56 -0500

    update exercises 2.x

--- src/main/scala/fpscala/Gettingstarted.scala
+++ src/main/scala/fpscala/Gettingstarted.scala
@@ -29,4 +29,24 @@ object Gettingstarted {
     go(0)
   }
 
+
+  /***
+   * exercise 2.3
+   */
+  def curry[A,B,C](f: (A, B) => C): A => (B => C) =
+    a => b => f(a, b)
+
+
+  /***
+   * exercise 2.4
+   */
+  def uncurry[A,B,C] (f: A => B => C): (A, B) => C =
+    (a, b) => f(a)(b)
+
+  /***
+   * exercise 2.5
+   */
+  def composee[A, B, C](f: B => C, g: A => B): A => C =
+    a => f(g(a))
+
 }
