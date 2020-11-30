2762777ac00f9b7995ed669718e0757c15eb0d98
Yi Shen <yshensh@gmail.com>
2020-11-29 23:03:25 -0500

    add comments

--- src/main/scala/fpscala/laziness/Stream.scala
+++ src/main/scala/fpscala/laziness/Stream.scala
@@ -47,14 +47,18 @@ case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
 
 object Stream {
+  // A smart constructor for creating a nonempty stream.
   def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+    // cache the head and tail as lazy values to avoid repeated evaluation.
     lazy val head = hd
     lazy val tail = tl
     Cons(() => head, () => tail)
   }
 
+  // A smart constructor for creating an empty stream of a particular type.
   def empty[A]: Stream[A] = Empty
 
+  // A convenient variable-argument method for constructing a Stream from multiple elements.
   def apply[A](as: A*): Stream[A] =
     if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
 }
