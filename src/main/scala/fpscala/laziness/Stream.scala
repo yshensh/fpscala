544c1f7fd0bb93f56fc7060ad1632bf998c31f80
Yi Shen <yshensh@gmail.com>
2020-11-29 22:56:18 -0500

    exercise 5.2 and 5.3

--- src/main/scala/fpscala/laziness/Stream.scala
+++ src/main/scala/fpscala/laziness/Stream.scala
@@ -1,6 +1,7 @@
 package fpscala.laziness
 
 import scala.collection.immutable.List
+import fpscala.laziness.Stream.{cons, empty}
 
 
 sealed trait Stream[+A] {
@@ -18,18 +19,28 @@ sealed trait Stream[+A] {
    * Exercise 5.2
    * Write a function take(n) for returning the first n elements of a Stream, and drop(n) for skipping the first n elements of a Stream.
    */
-  def take(n: Int): Stream[A]= ???
+  def take(n: Int): Stream[A]=
+    this match {
+      case Cons(h, t) if n > 1 => cons(h(), t().take(n - 1))
+      case Cons(h, _) if n == 1 => cons(h(), empty)
+      case _ => empty
+    }
 
-  def drop(n: Int): Stream[A]= ???
+  def drop(n: Int): Stream[A]=
+    this match {
+      case Cons(_, t) if n > 0 => t().drop(n-1)
+      case _ => this
+    }
 
   /**
    * Exercise 5.3
    * Write the function takeWhile for returning all starting elements of a Stream that match the given predicate.
    */
-  def takeWhile(p: A => Boolean): Stream[A] = ???
-
-
-
+  def takeWhile(f: A => Boolean): Stream[A] =
+    this match {
+      case Cons(h, t) if f(h()) => cons(h(), t() takeWhile f)
+      case _ => empty
+    }
 }
 case object Empty extends Stream[Nothing]
 // A nonempty stream consists of a head and a tail, which are both non-strict.
--- src/test/scala/fpscala/Laziness.scala
+++ src/test/scala/fpscala/Laziness.scala
@@ -1,6 +1,6 @@
 package fpscala
 
-import fpscala.laziness.Stream._
+import fpscala.laziness.{Stream}
 
 import scala.collection.immutable.List
 import org.scalatest.{FunSuite, Matchers}
@@ -8,7 +8,21 @@ import org.scalatest.{FunSuite, Matchers}
 
 class Laziness extends FunSuite with Matchers {
   test("exercise 5.1") {
-    laziness.Stream(1, 2, 3).toList shouldBe List(1, 2, 3)
+    Stream(1, 2, 3).toList shouldBe List(1, 2, 3)
   }
 
+  test("exercise 5.2") {
+    Stream("a", "b", "c").take(1).toList shouldBe List("a")
+    Stream("a", "b", "c").take(2).toList shouldBe List("a", "b")
+
+    Stream("a", "b", "c").drop(1).toList shouldBe List("b", "c")
+    Stream("a", "b", "c").drop(2).toList shouldBe List("c")
+  }
+
+  test("exercise 5.3") {
+    Stream(2, 1, 3).takeWhile(x => x % 2 == 0).toList shouldBe List(2)
+    Stream(1, 2, 3).takeWhile(x => x % 2 == 0).toList shouldBe List()
+    Stream(1, 2, 3).takeWhile(x => x % 2 != 0).toList shouldBe List(1)
+    Stream(1, 3, 2).takeWhile(x => x % 2 != 0).toList shouldBe List(1, 3)
+  }
 }
